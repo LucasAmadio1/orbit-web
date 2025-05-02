6cce23ad942216a6c949119473ee0b7c7c134a60
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-05-01 21:43:08 -0300

    pendingGoals and summary

---
+++ src/components/pending-goals.tsx
@@ -0,0 +1,32 @@
+import { useQuery } from '@tanstack/react-query'
+import { Plus } from 'lucide-react'
+import { getPendingGoals } from '../http/get-pending-goals'
+import { OutlineButton } from './ui/outline-button'
+
+export function PendingGoals() {
+  const { data: pendingGoals } = useQuery({
+    queryKey: ['pending-goals'],
+    queryFn: getPendingGoals,
+    staleTime: 1000 * 60, // 60 segundos
+  })
+
+  if (!pendingGoals) {
+    return null
+  }
+
+  return (
+    <div className="flex gap-3 flex-wrap">
+      {pendingGoals.map((goal) => {
+        return (
+          <OutlineButton
+            key={goal.id}
+            disabled={goal.completionCount >= goal.desiredWeeklyFrequency}
+          >
+            <Plus className="size-4 text-zinc-600" />
+            {goal.title}
+          </OutlineButton>
+        )
+      })}
+    </div>
+  )
+}
